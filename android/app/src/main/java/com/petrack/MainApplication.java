c134ecff32b1b98dbaa1aea07edba06779179871
jaydennleemc <jaydennleemc@gmail.com>
2019-04-24 01:05:40 +0800

    added qr code scanner lib

--- android/app/src/main/java/com/petrack/MainApplication.java
+++ android/app/src/main/java/com/petrack/MainApplication.java
@@ -3,6 +3,7 @@ package com.petrack;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 
@@ -40,6 +41,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RNCameraPackage(),
             new AsyncStoragePackage(),
             new RNCWebViewPackage(),
                     new FBSDKPackage(mCallbackManager),
--- src/app/screens/ScanPage.js
+++ src/app/screens/ScanPage.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react';
-
-import {View, StyleSheet, SafeAreaView, TouchableOpacity, Image, ImageBackground} from 'react-native';
+import {View, StyleSheet, SafeAreaView, TouchableOpacity, Image, ImageBackground, Text} from 'react-native';
 import {scale} from "react-native-size-matters";
 import * as colors from '../constants/colors';
 import Icon from 'react-native-vector-icons/AntDesign';
 import * as images from '../constants/images';
 import * as Animatable from 'react-native-animatable';
-import {Button} from "react-native-elements";
-import {Styles} from '../constants/styles';
 import {Actions} from "react-native-router-flux";
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class ScanPage extends Component {
+
+    onSuccess(e) {
+        Linking
+            .openURL(e.data)
+            .catch(err => console.error('An error occured', err));
+    }
+
     render() {
         return (
             <View style={{backgroundColor: colors.lightColor, flex: 1}}>
@@ -27,12 +32,16 @@ export default class ScanPage extends Component {
                     backgroundColor: colors.lightColor,
                     justifyContent: 'center',
                 }}>
-                    <ImageBackground source={images.Scan_square} style={{width: scale(200), height: scale(200),}}>
-                        <Animatable.View animation="fadeInDown" duration={1000} iterationCount={100}>
-                            <Image source={images.Scan_line} style={{width: scale(200), marginTop: scale(120)}}
-                                   resizeMode={'stretch'}/>
-                        </Animatable.View>
-                    </ImageBackground>
+
+                    <QRCodeScanner
+                        onRead={this.onSuccess.bind(this)}
+                    />
+                    {/*<ImageBackground source={images.Scan_square} style={{width: scale(200), height: scale(200),}}>*/}
+                    {/*    <Animatable.View animation="fadeInDown" duration={1000} iterationCount={100}>*/}
+                    {/*        <Image source={images.Scan_line} style={{width: scale(200), marginTop: scale(120)}}*/}
+                    {/*               resizeMode={'stretch'}/>*/}
+                    {/*    </Animatable.View>*/}
+                    {/*</ImageBackground>*/}
                 </View>
 
             </View>
@@ -40,4 +49,23 @@ export default class ScanPage extends Component {
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    centerText: {
+        flex: 1,
+        fontSize: 18,
+        padding: 32,
+        color: '#777',
+    },
+    textBold: {
+        fontWeight: '500',
+        color: '#000',
+    },
+    buttonText: {
+        fontSize: 21,
+        color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+        padding: 16,
+    },
+});
+
